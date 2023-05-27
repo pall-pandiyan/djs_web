4ea2e717a0c87e0d4813c67be5f63b74ce391689
Pall Pandiyan.S <pallpandiyan@gmail.com>
2023-05-27 23:45:18 +0530

    implemented login and logout views

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('login', views.Login.as_view(), name="login"),
+    path('login1', views.login_view, name="login1"),
+    path('logout', views.Logout.as_view(), name="logout"),
 ]
--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, resolve_url
+from django.contrib.auth.views import LoginView, LogoutView
 
-# Create your views here.
+from . import forms
+
+
+class Login(LoginView):
+    template_name = "accounts/login.html"
+
+    def get_default_redirect_url(self):
+        if self.next_page:
+            return resolve_url(self.next_page)
+        else:
+            return resolve_url("/")
+
+
+class Logout(LogoutView):
+    template_name = "accounts/logout.html"
+
+
+def login_view(request):
+    form = forms.LoginForm().render("form_snipet.html")
+    return render(
+        request=request,
+        template_name="accounts/login.html",
+        context={'form': form}
+    )
