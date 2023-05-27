6d870ed63b6a3be740581a817889fa670a6f4761
Pall Pandiyan.S <pallpandiyan@gmail.com>
2023-05-27 20:47:07 +0530

    let the gunicorn to run in the background

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-gunicorn --chdir ./src core.wsgi:application --keep-alive 600 --pid ../pid/gunicorn.pid --user rx7 --group rx7 --access-logfile ../log/gunicorn/access.log --error-logfile ../log/gunicorn/error.log --reload
+gunicorn --chdir ./src core.wsgi:application --keep-alive 600 --pid ../pid/gunicorn.pid --user rx7 --group rx7 --access-logfile ../log/gunicorn/access.log --error-logfile ../log/gunicorn/error.log --reload --daemon
